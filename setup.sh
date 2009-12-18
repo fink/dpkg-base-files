d087f4d09fa3e6d0fabb138a37db379f1d4d90ac
dmacks <dmacks@netspace.org>
2009-12-18 04:37:48 +0000

    missed one

---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/sh -e
+#
+# setup.sh - configure base-files package
+#
+# Fink - a package manager that downloads source and installs it
+# Copyright (c) 2001 Christoph Pfisterer
+# Copyright (c) 2001-2009 The Fink Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA.
+#
+
+if [ $# -ne 1 ]; then
+  echo "Usage: ./setup.sh <prefix>"
+  echo "  Example: ./setup.sh /sw"
+  exit 1
+fi
+
+basepath=$1
+
+echo "Creating scripts for $basepath..."
+for file in init.sh init.csh editor pager ; do
+  sed "s|BASEPATH|$basepath|g" <$file.in >$file
+done
+
+echo "Creating dpkg script wrapper scripts..."
+for file in preinst postinst prerm postrm ; do
+    sed "s|@PREFIX@|$basepath|g" <$file.in >$file
+done
+
+exit 0
